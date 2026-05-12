e37baec130fc750c926a8e040ccf2aba1f863409
claytantor <claytantor@gmail.com>
2026-05-12 00:44:10 -0700

    fix copy paste

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -21,6 +21,20 @@ opt.colorcolumn = "120"
 opt.splitbelow = true
 opt.splitright = true
 opt.mouse = "a"
-opt.clipboard = "unnamedplus"
+if vim.env.SSH_TTY ~= nil or vim.env.SSH_CONNECTION ~= nil or vim.env.DISPLAY == nil or vim.env.DISPLAY == "" then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+else
+  opt.clipboard = "unnamedplus"
+end
 opt.ignorecase = true
 opt.smartcase = true
