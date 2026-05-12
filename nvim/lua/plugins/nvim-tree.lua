043969ccf07c325466d6ff9dce1d551bfa28c5c6
claytantor <claytantor@gmail.com>
2026-05-12 07:35:35 -0700

    add vi support

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -7,6 +7,19 @@ return {
       { "<Leader>ef", "<Cmd>NvimTreeFocus<CR>", desc = "Focus file tree" },
       { "<Leader>er", "<Cmd>NvimTreeRefresh<CR>", desc = "Refresh file tree" },
     },
+    config = function(_, opts)
+      opts.on_attach = function(bufnr)
+        local api = require('nvim-tree.api')
+        api.config.mappings.default_on_attach(bufnr)
+        vim.keymap.set('n', '<Leader>vi', function()
+          local node = api.tree.get_node_under_cursor()
+          if node and node.type == 'file' then
+            vim.cmd('split | terminal vi ' .. vim.fn.shellescape(node.absolute_path))
+          end
+        end, { buffer = bufnr, noremap = true, silent = true, desc = 'Open in vi' })
+      end
+      require('nvim-tree').setup(opts)
+    end,
     opts = {
       disable_netrw = true,
       hijack_netrw = true,
